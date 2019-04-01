b52d52f056e3c8bcbe4ff9e2803076c14c24c230
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-04-01 13:35:34 +0100

    feat(types): add TypeScript decelerations

---
+++ types/lib/html-to-dom-client.d.ts
@@ -0,0 +1,9 @@
+import { DomElement } from 'domhandler';
+
+/**
+ * Parses HTML and reformats DOM nodes output.
+ *
+ * @param - Raw string of HTML to parse.
+ * @returns Parsed DomElements.
+ */
+export default function parseDOM(html: string): DomElement[];
---
+++ types/lib/utilities.d.ts
@@ -0,0 +1,26 @@
+import { DomElement } from 'domhandler';
+
+/**
+ * Format DOM attributes to an associative array.
+ *
+ * @param attributes - The list of attributes to format.
+ * @returns An object mapping attributes names to values.
+ */
+export function formatAttributes(attributes: NamedNodeMap): {[name: string]: string};
+
+/**
+ * Format the browser DOM nodes to mimic the output of `htmlparser2.parseDOM()`.
+ *
+ * @param nodes - The DOM nodes to format.
+ * @param parentObj - The formatted parent node of the given DOM nodes.
+ * @param directive - The directive.
+ */
+export function formatDOM(nodes: NodeList, parentObj?: DomElement, directive?: string): DomElement[];
+
+/**
+ * Detect IE with or without version.
+ *
+ * @param version The IE version to detect.
+ * @returns Whether IE or the version has been detected.
+ */
+export function isIE(version: number): boolean;
