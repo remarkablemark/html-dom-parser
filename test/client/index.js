a0c58aa6649d5ee429ffa20e8cb98e0d8070e6c6
Mark <mark@remarkablemark.org>
2019-11-03 14:01:31 -0500

    test(client): add tests for client parser that will be run by karma

---
+++ test/client/index.js
@@ -0,0 +1,21 @@
+var assert = window.chai.assert;
+var htmlCases = require('../cases/html');
+var htmlparser = require('../../dist/htmlparser2');
+var parser = require('../../lib/html-to-dom-client');
+var helpers = require('../helpers');
+
+describe('client parser', function() {
+  helpers.throwsError(parser, assert);
+  helpers.runTests(htmlCases, parser, htmlparser.parseDOM, assert);
+
+  it.skip('performance', function() {
+    var html = '<div>test</div>';
+    var start = performance.now();
+    var times = 1000;
+    while (--times) {
+      parser(html);
+    }
+    var end = performance.now();
+    console.log('performance: ' + (end - start) + ' milliseconds'); // eslint-disable-line no-console
+  });
+});
