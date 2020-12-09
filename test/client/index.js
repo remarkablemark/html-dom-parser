cb4bbd8e1f2f34f831138af31a26ed31f38045b6
Mark <mark@remarkablemark.org>
2020-12-08 22:32:12 -0500

    test(client): refactor Karma client tests and helpers

--- test/client/index.js
+++ test/client/index.js
@@ -1,19 +1,19 @@
 var assert = window.chai.assert;
 var htmlCases = require('../cases/html');
-var htmlparser = require('../../dist/htmlparser2');
-var parser = require('../../lib/html-to-dom-client');
+var serverParser = require('../../dist/htmlparser2').parseDOM;
+var clientParser = require('../../lib/html-to-dom-client');
 var helpers = require('../helpers');
 
 describe('client parser', function () {
-  helpers.throwsError(parser, assert);
-  helpers.runTests(htmlCases, parser, htmlparser.parseDOM, assert);
+  helpers.throwsError(clientParser, assert);
+  helpers.runTests(htmlCases, clientParser, serverParser, assert);
 
   it.skip('performance', function () {
     var html = '<div>test</div>';
     var start = performance.now();
     var times = 1000;
     while (--times) {
-      parser(html);
+      clientParser(html);
     }
     var end = performance.now();
     console.log('performance: ' + (end - start) + ' milliseconds'); // eslint-disable-line no-console
