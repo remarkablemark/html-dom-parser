04cf01cebcfb06f4ab64ea8e3def73af8a533f51
Mark <mark@remarkablemark.org>
2020-12-08 22:01:15 -0500

    test(karma): tidy Karma configuration and add comments

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,15 @@
+// Karma configuration
+// https://karma-runner.github.io/5.2/config/configuration-file.html
 module.exports = config => {
   config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai', 'commonjs'],
-    reporters: ['mocha'],
-    mochaReporter: {
-      showDiff: true
-    },
+
+    // list of files / patterns to load in the browser
     files: [
       'dist/htmlparser2.js',
       'lib/*.js',
@@ -12,12 +17,62 @@ module.exports = config => {
       'test/client/*.js',
       'test/helpers/*.js'
     ],
+
+    // list of files / patterns to exclude
     exclude: ['lib/html-to-dom-server.js'],
-    browsers: ['Chrome'],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'dist/**/*.js': ['commonjs'],
       'lib/**/*.js': ['commonjs'],
       'test/**/*.js': ['commonjs']
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha', 'progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    // Mocha reporter options
+    // https://www.npmjs.com/package/karma-mocha-reporter
+    mochaReporter: {
+      showDiff: true
     }
   });
 };
