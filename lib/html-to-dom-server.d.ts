2a748b8d4cc06660a18636e018706508f4fc19f4
Mark <mark@remarkablemark.org>
2020-12-13 17:09:20 -0500

    feat: upgrade `domhandler` to 3.3.0 and `htmlparser2` to 4.1.0
     domhandler   3.0.0  →  3.3.0
     htmlparser2  4.0.0  →  4.1.0
    
    domhandler: https://github.com/fb55/domhandler/tree/v3.3.0
    htmlparser2: https://github.com/fb55/htmlparser2/tree/v4.1.0
    
    Replace domhandler `DataNode` with `Comment` and `Text`.

--- lib/html-to-dom-server.d.ts
+++ lib/html-to-dom-server.d.ts
@@ -1,18 +1,24 @@
 // TypeScript Version: 4.1
 
-import { DataNode, DomHandlerOptions, Element } from 'domhandler';
+import {
+  Comment,
+  DomHandlerOptions,
+  Element,
+  ProcessingInstruction,
+  Text
+} from 'domhandler';
 
 /**
  * Parses HTML string to DOM nodes in Node.js.
  *
  * This is the same method as `require('htmlparser2').parseDOM`
- * https://github.com/fb55/htmlparser2/blob/v4.0.0/src/index.ts#L18-L22
+ * https://github.com/fb55/htmlparser2/blob/v4.1.0/src/index.ts#L18-L22
  *
  * @param  html    - HTML markup.
- * @param  options - Parser options (https://github.com/fb55/domhandler/tree/v3.0.0#readme).
- * @return         - DOM elements.
+ * @param  options - Parser options (https://github.com/fb55/domhandler/tree/v3.3.0#readme).
+ * @return         - DOM nodes.
  */
 export default function HTMLDOMParser(
   html: string,
   options?: DomHandlerOptions
-): Array<DataNode | Element>;
+): Array<Comment | Element | ProcessingInstruction | Text>;
