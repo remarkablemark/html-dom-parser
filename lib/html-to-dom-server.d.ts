994ffddaed92fabd32db1449e49a9c31d97ed663
Mark <mark@remarkablemark.org>
2020-12-13 00:06:52 -0500

    chore(html-to-dom-server): add JSDoc param names to declaration file

--- lib/html-to-dom-server.d.ts
+++ lib/html-to-dom-server.d.ts
@@ -8,9 +8,9 @@ import { DomHandlerOptions, DomElement } from 'domhandler';
  * This is the same method as `require('htmlparser2').parseDOM`
  * https://github.com/fb55/htmlparser2/blob/v3.9.1/lib/index.js#L39-L43
  *
- * @param  - HTML markup.
- * @param  - Parser options (https://github.com/fb55/domhandler/tree/v2.4.2#readme).
- * @return - DOM elements.
+ * @param  html    - HTML markup.
+ * @param  options - Parser options (https://github.com/fb55/domhandler/tree/v2.4.2#readme).
+ * @return         - DOM elements.
  */
 export default function HTMLDOMParser(
   html: string,
