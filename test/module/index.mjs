6f614babf9a860873d93a31817f1f261a78f173d
Mark <mark@remarkablemark.org>
2022-08-19 22:29:38 -0400

    test(module): add ES Module test for client html-dom-parser.mjs

--- test/module/index.mjs
+++ test/module/index.mjs
@@ -1,4 +1,6 @@
 import assert from 'assert';
 import HTMLDOMParser from '../../index.mjs';
+import HTMLDOMParserClient from '../../lib/client/html-to-dom.mjs';
 
 assert.strictEqual(typeof HTMLDOMParser, 'function');
+assert.strictEqual(typeof HTMLDOMParserClient, 'function');
