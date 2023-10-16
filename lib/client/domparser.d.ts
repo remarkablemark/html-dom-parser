32c606cc2726679ddd50daeae36d97667b603e1a
Mark <mark@remarkablemark.org>
2023-10-16 01:12:30 -0400

    chore(lib): remove build artifacts

--- lib/client/domparser.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Parses HTML string to DOM nodes.
- *
- * @param html - HTML markup.
- * @returns - DOM nodes.
- */
-export default function domparser(html: string): NodeList;
-//# sourceMappingURL=domparser.d.ts.map
--- lib/client/html-to-dom.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Parses HTML string to DOM nodes in browser.
- *
- * @param html - HTML markup.
- * @returns - DOM elements.
- */
-export default function HTMLDOMParser(html: string): (import("domhandler").Element | import("domhandler").Text | import("domhandler").Comment | import("domhandler").ProcessingInstruction)[];
-//# sourceMappingURL=html-to-dom.d.ts.map
