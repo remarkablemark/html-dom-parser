8ed55e23df5c50e19c6df5d0e4856f52ee890a0c
Mark <mark@remarkablemark.org>
2023-10-21 20:04:39 -0400

    chore: export types from index.ts
    Release-As: 5.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
  * When running on Node.js, use the server parser.
  * When bundling for the browser, use the client parser.
  *
- * @see {@link https://github.com/substack/node-browserify#browser-field}
+ * @see https://github.com/substack/node-browserify#browser-field
  */
 export { default } from './server/html-to-dom';
+
+export * from './types';
